49bcf49736610586663270ba7d4229eb5c7ae8e4
LiuWei <liuwei@LiuWeideMac-mini.local>
2015-12-05 20:15:02 +0800

    tijiaolba

---
+++ 新浪微博_swift/新浪微博_swift/class/Main/Compare/Controller/LWCompareController.swift
@@ -0,0 +1,35 @@
+//
+//  LWCompareController.swift
+//  新浪微博_swift
+//
+//  Created by LiuWei on 15/12/5.
+//  Copyright © 2015年 liuwei. All rights reserved.
+//
+
+import UIKit
+
+class LWCompareController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 新浪微博_swift/新浪微博_swift/class/Main/Compare/View/LWPlaceholdView.swift
@@ -0,0 +1,21 @@
+//
+//  LWPlaceholdView.swift
+//  新浪微博_swift
+//
+//  Created by LiuWei on 15/12/5.
+//  Copyright © 2015年 liuwei. All rights reserved.
+//
+
+import UIKit
+
+class LWPlaceholdView: UITextView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
